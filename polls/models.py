b5f2388eff5cf855327ba1ab43633a9b7179aafb
Himanshu Bansal <himanshub166@gmail,com>
2022-09-28 11:21:09 +0530

    september 28

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,44 @@
+from distutils.command.upload import upload
+import email
+from email.policy import default
+from turtle import width
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    sno = models.AutoField(primary_key=True)
+    image = models.ImageField(upload_to='polls/images',default="")
+    role = models.TextField()
+    resume = models.FileField(upload_to='polls/files',default="")
+
+    def __str__(self):
+        return self.role
+
+class Skill(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name=models.CharField(max_length=111)
+    width=models.CharField(max_length=111)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    sno = models.AutoField(primary_key=True)
+    pic = models.ImageField(upload_to='polls/images',default="")
+    name = models.CharField(max_length=111)
+    domain = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    sno = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=200)
+    name = models.CharField(max_length=111)
+    subject = models.CharField(max_length=500)
+    message = models.TextField()
+
+    def __str__(self) -> str:
+        return self.email
+
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render,HttpResponse,redirect
-
+from .models import Service,Skill,Project
 # Create your views here.
 
 def home(request):
-    return HttpResponse('hello world')
+    allpost = Service.objects.all()
+    skills = Skill.objects.all()
+    project = Project.objects.all()
+    return render(request,'polls/home.html',{'allpost':allpost,'skills':skills,'project':project})
+
+
+# def skill(request):
+#     skills = Skill.objects.all()
+#     return render(request,'polls/home.html',{'skills':skills})
